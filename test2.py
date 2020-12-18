477b99aaff8fe648edee50bb386149eedd455284
김도훈 <dohunkim@gimdohun-ui-MacBookPro.local>
2020-12-18 21:48:10 +0900

    list comprehension

--- test2.py
+++ test2.py
@@ -1,2 +1,16 @@
-result = [i for i in range(10) if i%2==0]
-print(result)
+# result = [i for i in range(10) if i%2==0]
+# print(result)
+
+# word_1 = ["A","B","C"]
+# word_2 = ["D","E","A"]
+#
+# result = [i+j for i in word_1 for j in word_2 if (i!=j)]
+# result.sort()
+#
+# print(result)
+
+word_1 = 'Someday at ChristMas'.split()
+stuff = [[w.upper(), w.lower(), len(w)] for w in word_1]
+
+for i in stuff:
+    print(i)
