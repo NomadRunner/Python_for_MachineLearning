427c76e84293ca64c5554138fa00749011da3633
김도훈 <dohunkim@gimdohun-ui-MacBookPro.local>
2020-12-18 20:24:06 +0900

    pythonic split

--- my_first_python.py
+++ my_first_python.py
@@ -1,3 +1,7 @@
-colors = ['red','green','black', 'blue']
-result = ''.join(colors)
-print(result)
+# colors = ['red','green','black', 'blue']
+# result = ''.join(colors)
+# print(result)
+
+example = 'cs5.world.edu'
+subdomain,domain,tld = example.split('.')
+print(subdomain,domain,tld)
